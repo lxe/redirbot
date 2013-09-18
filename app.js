f162e2e0dcbb9cf9fda619606b136eb170b57ac4
Aleksey Smolenchuk <lxe@lxe.co>
2013-09-18 14:30:22 -0400

    Compacting the links

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.get('/api', function(req, res) {
       return res.json(response.request.redirects);
     }
 
-    res.json(cheerio.load(body, { 
+    res.json(_.compact(cheerio.load(body, { 
       lowerCaseTags : true 
     })('a').map(function() {
       return this.attr('href')
-    }));
+    })));
   })
 });
 
