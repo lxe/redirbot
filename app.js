51471ac388751a610a989dec8a5f2defa3cd9a62
Aleksey Smolenchuk <lxe@lxe.co>
2013-09-18 13:44:00 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var _ = require('lodash')
+  , express = require('express')
+  , request = require('request')
+  , cheerio = require('cheerio')
+  , async   = require('async')
+  , port    = 8888;
+
+var app = module.exports = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api', function(req, res) {
+  request.get({
+    uri : req.query.uri,
+    followAllRedirects : true
+  }, function(error, response, body) {
+    if (error) 
+      return res.json(400, error);
+
+    if (req.query.redirects) {
+      return res.json(response.request.redirects);
+    }
+
+    res.json(cheerio.load(body, { 
+      lowerCaseTags : true 
+    })('a').map(function() {
+      return this.attr('href')
+    }));
+  })
+});
+
+app.listen(port, function() {
+  console.log('Listening on :%d', port);
+});
